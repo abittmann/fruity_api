9770b09a862f42e98a42b4f844105ee72059d24c
Alexandra Bittmann <alexandra.bittmann@gmail.comgit config --global user.name abittmann>
2025-04-08 09:21:26 +0100

    add .env file and variables

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config() // load environment variables from .env file or code can read what is in the .env file.
+
+
 const app = require('./app')
-const port = 3000;
+const port = process.env.PORT  // port number to listen to. If the port is not set in the environment variable, it will use 3000 as default.
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`); // binding the port with the listen method - listening for connections. log a message to know if it is working.
